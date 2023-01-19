65972e17af40d22b45020a2490e1fb90a2069887
VishalOmprasad <vishal.omprasad@syncfusion.com>
2023-01-19 20:22:48 +0530

    Updated sample with button and behavior

---
+++ SelectionShape/SelectionShape/Behavior/CalendarBehavior.cs
@@ -0,0 +1,45 @@
+﻿using Syncfusion.Maui.Calendar;
+
+namespace SelectionShape
+{
+    public class CalendarBehavior : Behavior<ContentPage>
+    {
+        private SfCalendar calendar;
+        private Button selectionShapeButton;
+
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.calendar = bindable.FindByName<SfCalendar>("calendar");
+            this.selectionShapeButton = bindable.FindByName<Button>("selectionShapeButton");
+            this.selectionShapeButton.Text = "SelectionShape: " + this.calendar.SelectionShape.ToString();
+            this.selectionShapeButton.Clicked += SelectionShapeButton_Clicked;
+        }
+
+        private void SelectionShapeButton_Clicked(object sender, EventArgs e)
+        {
+            if (this.calendar.SelectionShape == CalendarSelectionShape.Circle)
+            {
+                this.calendar.SelectionShape = CalendarSelectionShape.Rectangle;
+                this.selectionShapeButton.Text = "SelectionShape: " + this.calendar.SelectionShape.ToString();
+            }
+            else
+            {
+                this.calendar.SelectionShape = CalendarSelectionShape.Circle;
+                this.selectionShapeButton.Text = "SelectionShape: " + this.calendar.SelectionShape.ToString();
+            }
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            if (this.selectionShapeButton != null)
+            {
+                this.selectionShapeButton.Clicked -= SelectionShapeButton_Clicked;
+            }
+
+            this.selectionShapeButton = null;
+            this.calendar = null;
+        }
+    }
+}
